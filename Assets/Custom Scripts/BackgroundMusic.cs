a6464f4a31f284ba4397c0341bd9cd555743b28c
AlteriusOmega <TheRealSamChaney@gmail.com>
2023-10-26 19:02:57 -0500

    Created new script BackgroundMusic which I added to the BG Music object. It simply makes the Background music class singleton effectively. It makes an instance of BackgroundMusic class and sets it to null then overrides the Awake method. If the instance is null it sets the instance to this and calls DontDestroyOnLoad(gameObject), otherwise it calls Destroy on gameObject ensuring there can only be one. Also Renamed Scripts folder to Custom Scripts to avoid confusion.

---
+++ Assets/Custom Scripts/BackgroundMusic.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    private static BackgroundMusic instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
